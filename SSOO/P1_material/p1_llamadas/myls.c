52d5c4c2823fb3fb3a7d0772c4d443a82ce13ea4
juanbanpar <100383373@alumnos.uc3m.es>
2019-02-20 19:49:27 +0100

    Start working with on myls

--- SSOO/P1_material/p1_llamadas/myls.c
+++ SSOO/P1_material/p1_llamadas/myls.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>              /* Cabecera llamada al sistema printf  */
+#include <stdio.h>              /* Cabecera llamada al sistema printf  */
 #include <unistd.h>             /* Cabecera llamada al sistema gtcwd  */
 #include <sys/types.h>          /* Cabeceras llamadas al sistema opendir, readdir y closedir  */
 #include <dirent.h>
@@ -7,5 +7,19 @@ nclude <stdio.h>              /* Cabecera llamada al sistema printf  */
 
 int main(int argc, char *argv[])
 {
-
+    
+    if(argc < 2){              /*If arguments are lower than two it means that we are working with the actual directory*/
+        
+        
+        
+    } else if (argc > 2){       /*It can't have more than two arguments*/
+        
+        printf("Error: introduce only one argument\n");
+        
+    } else {                    /*It means that we are working with an introduced directory*/
+        
+        
+        
+    }
+    
 }
