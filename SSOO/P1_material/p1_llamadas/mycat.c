447a2175931c42fa3e6929167b0edd4015d0016a
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-02-20 15:42:57 +0100

    mycat 2.0

--- SSOO/P1_material/p1_llamadas/mycat.c
+++ SSOO/P1_material/p1_llamadas/mycat.c
@@ -1,11 +1,38 @@
-#include <stdio.h>              /* Cabecera llamada al sistema printf  */
-#include <sys/types.h>          /* Cabeceras llamada al sistema open  */
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>             /* Cabecera llamadas al sistema read, write y close  */
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
 
+int main (int argc, char *argv[]){                      //argc is the number os parameters, argv are all the strings typed in the console    
+    
+    char file_buffer [1024];
+    ssize_t ro_bytes;
+    
+    if(argc != 2){                                      //Check if the user has typed a file to open
+        printf("Error: you need to enter a file\n");
+        return -1;
+    }
 
-int main(int argc, char *argv[])
-{
+    int openedFile = open(argv[1], O_RDONLY);           //Open the typed file only with read permissions
 
+    if(openedFile == -1){                               //Check if the file exists
+        
+        printf("Error: the file doesn't exit\n");
+        return -1;
+        
+    } else {
+        
+        ro_bytes = read(openedFile, file_buffer, 1024);
+        close(openedFile);
+        
+        if(ro_bytes == 0){
+            printf("The file is empty \n");
+            return 0;
+        } else {
+            printf("%s", file_buffer);
+        }
+    }
+
+    return 0;
 }
