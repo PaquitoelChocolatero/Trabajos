75692587e57193fc7b8f9e1c25559906a8eb4ce0
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-02-22 10:50:28 +0100

    mycat 4

--- SSOO/P1_material/p1_llamadas/mycat.c
+++ SSOO/P1_material/p1_llamadas/mycat.c
@@ -6,6 +6,8 @@
 
 int main (int argc, char *argv[]){                      //argc is the number os parameters, argv are all the strings typed in the console    
     
+    const int BUFF = 1024;
+
     if(argc != 2){                                      //Check if the user has typed a file to open
         printf("Error: you need to enter a file\n");
         return -1;
@@ -13,26 +15,22 @@ int main (int argc, char *argv[]){                      //argc is the number os 
 
     int openedFile = open(argv[1], O_RDONLY);           //Open the typed file only with read permissions
 
-    struct stat buf;                                    //Functions imported in order to extract the size of the whole file
-    fstat(openedFile, &buf);
-    off_t size = buf.st_size;
-
-    char file_buffer [size];                            //Text itself, the buffer is the same size as the file
-    ssize_t nbytes;                                     //Size of the text in bytes
-
-    if(openedFile == -1){                               //Check if the file exists
+    char file_buffer [BUFF];                            //Text itself, the buffer is the same size as the file
+    ssize_t nbytes = read(openedFile, file_buffer, BUFF);//Bytes read each time
+    
+    if(nbytes == 0){                                //Check if the file is empty
+        printf("The file is empty\n");
+        return 0;
+    }else if(openedFile == -1){                               //Check if the file exists
         printf("Error: the file doesn't exit\n");
         return -1;
-    } else {
-        nbytes = read(openedFile, file_buffer, size);
-        close(openedFile);
-        
-        if(nbytes == 0){                                //Check if the file is empty
-            printf("The file is empty\n");
-            return 0;
-        } else {
-            printf("%s", file_buffer);                  //Print the file
-        }
     }
+
+    do{
+        write(STDOUT_FILENO, file_buffer, nbytes);                  //Print the file
+        nbytes = read(openedFile, file_buffer, BUFF);
+    }while(nbytes != 0);
+    
+    close(openedFile);
     return 0;
 }
