d2ac84bbef566dfdcfad84de4a8216b11045286f
Juan Banga Pardo <100383373@alumnos.uc3m.es>
2019-02-22 10:07:36 +0100

    Now OURls detects directories

--- SSOO/P1_material/p1_llamadas/myls.c
+++ SSOO/P1_material/p1_llamadas/myls.c
@@ -7,19 +7,28 @@
 
 int main(int argc, char *argv[])
 {
-    
+    char actual_dir[PATH_MAX];
+
     if(argc < 2){              /*If arguments are lower than two it means that we are working with the actual directory*/
-        
-        
+
+	if (getcwd(actual_dir, sizeof(actual_dir)) != NULL) {
+		printf("Current working dir: %s\n", actual_dir);
+	} else {
+		perror("getcwd() error");
+		return 1;
+   	}
+	return 0;
         
     } else if (argc > 2){       /*It can't have more than two arguments*/
         
         printf("Error: introduce only one argument\n");
+        return 0;
         
     } else {                    /*It means that we are working with an introduced directory*/
         
-        
-        
+        getcwd(actual_dir, sizeof(actual_dir));
+	strcat(actual_dir, argv[1]);
+        printf("Current working dir: %s\n", actual_dir);
     }
     
 }
