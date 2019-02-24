7a8116480c79ab3c5cf4111fe427a2466a35d0c5
juanbanpar <100383373@alumnos.uc3m.es>
2019-02-24 14:27:42 +0100

    Fix code styling

--- SSOO/P1_material/p1_llamadas/myls.c
+++ SSOO/P1_material/p1_llamadas/myls.c
@@ -13,15 +13,19 @@ int main(int argc, char *argv[])
 
     if(argc < 2){              /*If arguments are lower than two it means that we are working with the actual directory*/
 
-	if (getcwd(actual_dir, sizeof(actual_dir)) != NULL) {
-		opened_dir = opendir(actual_dir);
-		while ((dir = readdir(opened_dir)) != NULL) {
- 			printf("%s\n", dir->d_name);
-		}
-	} else {
-		perror("getcwd() error");
-   	}
-	return 0;
+        if (getcwd(actual_dir, sizeof(actual_dir)) != NULL) {
+            
+            opened_dir = opendir(actual_dir);
+            while ((dir = readdir(opened_dir)) != NULL) {
+                printf("%s\n", dir->d_name);
+            }
+            
+        } else {
+            
+            perror("getcwd() error");
+        
+        }
+        return 0;
         
     } else if (argc > 2){       /*It can't have more than two arguments*/
         
@@ -29,12 +33,13 @@ int main(int argc, char *argv[])
         return 0;
         
     } else {                    /*It means that we are working with an introduced directory*/
+        
         getcwd(actual_dir, sizeof(actual_dir));
-	strcat(actual_dir, argv[1]);
-	opened_dir = opendir(actual_dir);
-	while ((dir = readdir(opened_dir)) != NULL) {
- 		printf("%s\n", dir->d_name);
-	}
+        strcat(actual_dir, argv[1]);
+        opened_dir = opendir(actual_dir);
+        while ((dir = readdir(opened_dir)) != NULL) {
+            printf("%s\n", dir->d_name);
+        }
     }
     closedir(opened_dir);
 }
