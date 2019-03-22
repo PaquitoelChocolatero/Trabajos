8de4d15f9bccf4c8f64c842b672bb6cc439018ab
juanbanpar <100383373@alumnos.uc3m.es>
2019-03-23 00:13:22 +0100

    still trying to fix mytime

--- SSOO/P2_material/msh.c
+++ SSOO/P2_material/msh.c
@@ -53,51 +53,42 @@ int main(void) {
             struct timeval ti, tf;
             long elapsed;
             gettimeofday(&ti, 0);
-            
-            printf("prueba");
-            
+                        
             pid = fork();
             if (pid == 0) {
                 
-                printf("%c", filev[0]);
-                char* filev_chopped = filev + 7;
-                printf("%c", filev_chopped[0]);
-                
                 //If there is an entry we close the current one and redirect to the new one
-                if (filev_chopped[0] != NULL) {
+                if (filev[0] != NULL) {
                     close(0);
-                    open(filev_chopped[0], O_RDONLY);
+                    open(filev[0], O_RDONLY);
                 }
                 
                 //The same but printing on screen
-                if (filev_chopped[1] != NULL) {
+                if (filev[1] != NULL) {
                     close(1);
-                    open(filev_chopped[1], O_CREAT|O_TRUNC|O_RDONLY);
+                    open(filev[1], O_CREAT|O_TRUNC|O_RDONLY);
                 }
                 
                 //The same but with the erros.
-                if (filev_chopped[2] != NULL) {
+                if (filev[2] != NULL) {
                     close(2);
-                    open(filev_chopped[2], O_CREAT|O_TRUNC|O_RDONLY);
+                    open(filev[2], O_CREAT|O_TRUNC|O_RDONLY);
                 }
 
                 //Print child PID
                 printf("Hijo: %i\n\n", pid);
                 //Do the exec
-                execvp(argvv[0][0], argvv[0]);
+                execvp(argvv[1][0], argvv[1]);
             }
 
             //Parent code, so wait for the child to finish
             //if its the only one in the order
-            if (bg == 0) {
+            if (!bg) {
                 printf("\nEsperando al hijo: %i\n", pid);
                 waitpid(pid, NULL, 0);
             }
-            else if (bg == 1){
-                bg = 0;
-                execvp(argvv[0][0], argvv[0]);
-            }
-            
+            else if (bg) bg = 0;
+
             else printf("[%i]\n", pid);
             
             gettimeofday(&tf, 0);
@@ -116,7 +107,7 @@ int main(void) {
         /*
          *  NORMAL COMMANDS
          */	
-        } else if(num_commands > 1){
+        /*} else if(num_commands > 1){
             //Go command by command
             for(int i=0; i<num_commands; i++){
                 //Create a pipe
@@ -163,7 +154,7 @@ int main(void) {
                     //Wait for the child to die
                     waitpid(pid, NULL, 0);
                 }
-            }
+            }*/
         } else if (num_commands == 1) {
             //Do the fork
             pid = fork();
