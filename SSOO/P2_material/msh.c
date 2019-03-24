b36e6a082491b92429174710c7ce58ff597f9c21
juanbanpar <100383373@alumnos.uc3m.es>
2019-03-24 13:31:31 +0100

    mytime almost almost there

--- SSOO/P2_material/msh.c
+++ SSOO/P2_material/msh.c
@@ -53,30 +53,30 @@ int main(void) {
         if (strncmp("mytime", argvv[0][0], 6) == 0) {
             gettimeofday(&start, NULL);
             
+            //Do the fork
             pid = fork();
             if (pid == 0) {
-                
+
                 //If there is an entry we close the current one and redirect to the new one
                 if (filev[0] != NULL) {
                     close(0);
                     open(filev[0], O_RDONLY);
                 }
-                
                 //The same but printing on screen
                 if (filev[1] != NULL) {
                     close(1);
-                    open(filev[1], O_CREAT|O_TRUNC|O_RDONLY);
+                    open(filev[1], O_CREAT|O_WRONLY);
                 }
-                
-                //The same but with the erros.
+                //The same but with the errors.
                 if (filev[2] != NULL) {
                     close(2);
-                    open(filev[2], O_CREAT|O_TRUNC|O_RDONLY);
+                    open(filev[2], O_CREAT|O_WRONLY);
                 }
                 
                 for (int i = 0; i < sizeof(argvv); i++) {
                     argvv[0][i] = argvv[0][i+1];
                 }
+                
                 //Do the exec
                 execvp(argvv[0][0], argvv[0]);
             }
@@ -84,7 +84,6 @@ int main(void) {
 
             gettimeofday(&end, NULL);
             printf("Time spent: %f secs.\n", (double)((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec))/1000000);
-
             
         } else if (strncmp("mypwd", argvv[0][0], 5) == 0){
             
@@ -149,8 +148,8 @@ int main(void) {
                     printf("[%d]\n", pid);
                     bg = 0;
                 }
-
             }
+            
         }else if (num_commands == 1) {
             //Do the fork
             pid = fork();
@@ -184,9 +183,8 @@ int main(void) {
             }
         }
 
-    }//fin while 
+    }//end while 
 
 	return 0;
 
 } //end main
-
