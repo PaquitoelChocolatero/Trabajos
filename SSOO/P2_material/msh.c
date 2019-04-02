e57a3a8cb325dd14bba6515303daa4d2f1934ad7
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-04-03 00:02:52 +0200

    Me estoy estresando

--- SSOO/P2_material/msh.c
+++ SSOO/P2_material/msh.c
@@ -33,44 +33,77 @@ void background(){
 }
 
 //Redirect either to Standard Output(and error), Standard Input or all of them
-void redirect(char* type){
-    if(strncmp(type, "in", 2)){
+void redirect(int type){
+    if(type==0){
         //If there is an entry we close the current one and redirect to the new one
         if (filev[0] != NULL) {
             close(0);
             open(filev[0], O_RDONLY);
         }
-    }else if(strncmp(type, "out", 3)){
+    }else if(type==1){
         //The same but printing on screen
         if (filev[1] != NULL) {
             close(1);
-            open(filev[1], O_CREAT|O_WRONLY);
+            open(filev[1], O_CREAT|O_TRUNC|O_WRONLY);
         }
         //Error output
         if (filev[2] != NULL) {
             close(2);
-            open(filev[2], O_CREAT|O_WRONLY);
+            open(filev[2], O_CREAT|O_TRUNC|O_WRONLY);
         }
-    }else{ //Used with "all"
+    }else{ //When redirecting everything
         if (filev[0] != NULL) {
             close(0);
             open(filev[0], O_RDONLY);
         }
         if (filev[1] != NULL) {
             close(1);
-            open(filev[1], O_CREAT|O_WRONLY);
+            open(filev[1], O_CREAT|O_TRUNC|O_WRONLY);
         }
         if (filev[2] != NULL) {
             close(2);
-            open(filev[2], O_CREAT|O_WRONLY);
+            open(filev[2], O_CREAT|O_TRUNC|O_WRONLY);
         }
     }
+}
+
+//Look for the current working directory
+void pwd(){
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("Current dir: %s\n", cwd);
+    } else {
+        perror("​Mypwd error​");
+    }
+}
+
+//myTime function that returns the time spent in a command
+double myTime(){
+
+    gettimeofday(&start, NULL);
+
+    //Do the fork
+    pid = fork();
+    if (pid == 0) {
+        redirect(2); 
+        //Do the exec
+        execvp(argvv[0][1], argvv[0]+1);
+    }
+
+    wait(NULL);
+
+    gettimeofday(&end, NULL);
+
+    //Calculate the final time spent
+    return (double)((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec))/1000000;
 }
 
 int main(void) {
     
 	setbuf(stdout, NULL);			/* Unbuffered */
 	setbuf(stdin, NULL);
+
+    int father = getpid();
 
 	while (1) {
 
@@ -84,46 +117,18 @@ int main(void) {
 /*************************************************     SPECIAL COMMANDS     **************************************************************************************************************************/
         
         //MYTIME
-        if (strncmp("mytime", argvv[0][0], 6) == 0) {
-            
-            gettimeofday(&start, NULL);
-            
-            //Do the fork
-            pid = fork();
-            if (pid == 0) {
-                redirect("all"); 
-                //Do the exec
-                execvp(argvv[0][1], argvv[0]+1);
-            }
-
-            wait(NULL);
-            
-            gettimeofday(&end, NULL);
-                
-            //Calculate the final time spent
-            double final_time = (double)((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec))/1000000;
-            printf("Time spent: %f secs.\n", final_time);
-        } 
+        if (strncmp("mytime", argvv[0][0], 6) == 0) printf("Time spent: %f secs.\n", myTime());
+        
         //MYPWD
-        else if (strncmp("mypwd", argvv[0][0], 5) == 0){
-            
-            char cwd[PATH_MAX];
-            if (getcwd(cwd, sizeof(cwd)) != NULL) {
-                printf("Current dir: %s\n", cwd);
-            } else {
-                perror("​Mypwd error​");
-            }
-        }
+        else if (strncmp("mypwd", argvv[0][0], 5) == 0) pwd();
 
 /*************************************************     NORMAL COMMANDS     **************************************************************************************************************************/
         
         else if(num_commands > 1){
-            
-            printf("%i\n", num_commands);
 
             //Each command has its pipe inside this array of pipes
             int p[num_commands-1][2];
-            
+
             //Go command by command
             for(int i=0; i<num_commands; i++){
                 
@@ -142,23 +147,24 @@ int main(void) {
                     //First command
                     if(i==0) {
                         dup2(p[i][1], STDOUT_FILENO);//Write
-                        redirect("in");    
+                        redirect(0);
                     }
                     //Last command
                     else if(i==num_commands-1){
                         dup2(p[i-1][0], STDIN_FILENO);//Read
-                        redirect("out"); 
+                        redirect(1); 
                     }
                     //Middle commands
                     else{
                         dup2(p[i-1][0], STDIN_FILENO);//Read
                         dup2(p[i][1], STDOUT_FILENO);//Write
                     }
+                    printf("%i\n", i);
                     //Exec
 				    execvp(argvv[i][0], argvv[i]);
                 }   
             }
-            if(pid!=0){
+            if(getpid()==father){
                 for(int i=0; i<num_commands; i++){
                     close(p[i][0]);
                     close(p[i][1]);
@@ -171,7 +177,7 @@ int main(void) {
             pid = fork();
             if (pid == 0) {
                 //Redirect with every standard input/output
-                redirect("all");
+                redirect(2);
                 //Run the command
                 execvp(argvv[0][0], argvv[0]);
             }
