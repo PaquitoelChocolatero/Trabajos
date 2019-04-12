88fd4ae36937ab282c73182740faae7d3fc3833b
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-04-12 19:05:41 +0200

    Ejercicio 1a

---
+++ SSOO/BashScripts/ejercicio1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+while [[ "$1" ]]; do
+	#Comprobamos que los usuarios introducidos no contengan : para usarlo en el grep
+	[[ "$1" =~ : ]] && echo "Los usuarios no pueden contener ':'" && continue
+
+	#Variable que recoge el interprete de cada usuario utilizando el regexp: empieza por el nombre del usuario y seguido de :
+	shell="$(grep  "^$1:" "/etc/passwd"|cut -d:  -f 7)"
+	
+	#Si existe la variable, es decir, existe el usuario imprime con el formato deseado
+	if [[ "$shell" ]]; then
+		echo "$1: $shell"
+
+	#Si no existe imprimimos un mensaje de error
+	else
+		echo "-ERROR- $1: no such user"
+	fi
+	
+	#Desplazamos los usuarios a la izquierda
+	shift
+done
+
--- SSOO/threads.c
+++ SSOO/threads.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 
-void * print(){
-    fprintf(stdout, "¡Hola, soy un hilo!\n");
+void * Funcion(void* arg){
+    int solution=*((int*) argv[1]);
+	for(int i = 2 ; i < size ; i++){
+    	solution-=*((int*)argv[i]);
+    }    
+    fprintf(stdout, "%i\n", solution);
 }
 
 int main(){
     pthread_t hilo;
-    pthread_create(&hilo, NULL, print, NULL);
+    pthread_create(&hilo, NULL, Funcion, &Funcion);
     pthread_exit(pthread_self());
     return 0;
 }
