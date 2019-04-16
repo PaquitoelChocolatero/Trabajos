fa8ce04f7a51985069e0f30e276e3cadacd5694b
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-04-16 18:20:14 +0200

    Comentarios

--- SSOO/BashScripts/exercise5.sh
+++ SSOO/BashScripts/exercise5.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+#Multiple choice
 WHAT=$(dialog --backtitle $0 --output-fd 1 --checklist 'Select items to uncompress:\n\nSPACE: check/uncheck     TAB: changes focus     INTRO: accept\nMouse can also be used, if supported by your terminal emulator.' 16 76 26 README 'Read this before doing anything else' off ChangeLog 'History of changes' off usr/include/ 'C/C++ headers' off usr/share/backgrounds/ 'Backgrounds for X desktop environments' off usr/share/pixmaps/ 'X11 pixmaps' off usr/share/man/ 'Manual pages' off)
 SIZE=$(stat --printf=%s $0)
+#If at least one choice has been answered
 if [ -n "$WHAT" ]; then
+  #Input redirects to /tmp/input.$$ file
   dialog --inputbox "Enter a directory:" 8 60 2>/tmp/input.$$
+  #The variable takes its value from reading the answer
   FILE="$(cat /tmp/input.$$)"
+  #Uncompressing the selected files
   (tail -n +8 $0 | pv -n -s $SIZE -i 0.25 | base64 -d | tar -xzf - $WHAT -V="$FILE") 2>&1 | dialog --gauge 'Uncompressing...' 8 46 
 fi
+#Deleting the temporary file created for the variable
 rm -f /tmp/input.$$
 exit 0;
+#Buffer for the base64 command
 H4sIAAAAAAAAA+3UwU6DQBAGYK7yFONJjbFhKVD11mg1Rr009e62jLBmZQkLqfTpXWptNMb0VJsm
 /3dZAsMMC8yMR8Prx5G3VYGTRFG3ikEcfF+XRBx5oh/1RRwmQeLOi1BEiUfBdh/rU2NrWRF5r1Kb
 khfZX3Gbru+pSa4sjZc/Ac2lJVXMdJNy6g6ozplMpTJVSE3P/M7VTFmOezY/ohel2QXImlrTUGrm
