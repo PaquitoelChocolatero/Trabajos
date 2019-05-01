1225e4260029e4e6fc394d3d0a9ba0e64b26f3a6
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-05-01 19:38:03 +0200

    Casi casi broker

--- SSOO/P3_material/lib/concurrency_layer.c
+++ SSOO/P3_material/lib/concurrency_layer.c
@@ -11,28 +11,26 @@ void destroy_concurrency_mechanisms(){
 }
 
 void* broker(void * args){
-  struct broker_info info;
+  struct broker_info * info;
   args = &info;
-  //Definiciones
-  char file[256] = args->batch_file;
-  struct stock_market * myMarket = args->market;
+  char* file[256];
+  *file = info->batch_file;
+  stock_market * myMarket = info->market;
+
   struct operations_queue * myQueue = myMarket->stock_operations;
-  //Creacion de puntero
-  iterator * it = new_iterator(file);
-  //Variables de la operacion
+  iterator * it = new_iterator(*file);
+
   int operation;
-  char id; //Accion sobre la que se hace la operacion
+  char id[11]; //Accion sobre la que se hace la operacion
   int type; //0->BUY  1->SELL
   int num_shares; //Numero de acciones
   int price; //Precio por accion
-  //Lectura reiterada del archivo
-  do{
-    int next_op = next_operation(it, &id, &type, &num_shares, &price);
-    struct operation * op;
-    new_operation(op, &id, type, num_shares, price);
+  struct operation * op;
+
+  while(next_operation(it, id, &type, &num_shares, &price) != -1){
+    new_operation(op, id, type, num_shares, price);
     enqueue_operation(myQueue, op);
-  }while(operation != -1);
-  //Destruccion del puntero
+  }
   destroy_iterator(it);
 }
 
