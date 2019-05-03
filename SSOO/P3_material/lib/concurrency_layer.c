c669c7332ce4580b731e0920cdc6e9e3bc196655
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-05-03 02:49:16 +0200

    No he hecho nada

--- SSOO/P3_material/lib/concurrency_layer.c
+++ SSOO/P3_material/lib/concurrency_layer.c
@@ -5,29 +5,18 @@
 pthread_mutex_t stock_operations;
 pthread_mutex_t stat_readers;
 
-//Concurrency conditions for each rol
-pthread_cond_t n_brokers;
-pthread_cond_t n_executers;
-pthread_cond_t n_readers;
-
 void init_concurrency_mechanisms(){
 
     //Initialize mutex
     pthread_mutex_init(&stock_operations, NULL);
     pthread_mutex_init(&stat_readers, NULL);
 
-    //Initialize conditions
-    pthread_cond_init(&n_brokers, NULL);
-    pthread_cond_init(&n_executers, NULL);
-    pthread_cond_init(&n_readers, NULL);
 }
 
 void destroy_concurrency_mechanisms(){
     
     pthread_mutex_destroy(&stock_operations);
     pthread_mutex_destroy(&stat_readers);
-    pthread_cond_destroy(&n_executers);
-    pthread_cond_destroy(&n_readers);
 }
 
 void* broker(void * args){
@@ -53,8 +42,7 @@ void* operation_executer(void * args){
     
     struct exec_info *info;
     info = args;
-    int* myexit;
-    *myexit = info->exit;
+    int* myexit = info->exit;
     stock_market * myMarket = info->market;
     pthread_mutex_t *myexit_mutex = info->exit_mutex;
     
@@ -75,8 +63,7 @@ void* stats_reader(void * args){
 
     struct reader_info *info;
     info = args;
-    int* myexit;
-    *myexit = info->exit;
+    int* myexit = info->exit;
     stock_market * myMarket = info->market;
     pthread_mutex_t *myexit_mutex = info->exit_mutex;
     int myfreq = info->frequency;
