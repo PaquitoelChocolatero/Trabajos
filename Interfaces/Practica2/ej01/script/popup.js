28df4a8ebb5631c23558e726a065cf33c0f26c11
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-10-17 00:40:51 +0200

    Trying to remove...

--- Interfaces/Practica2/ej01/script/popup.js
+++ Interfaces/Practica2/ej01/script/popup.js
@@ -1,12 +1,6 @@
 $(document).ready(function(){
     var modal = document.querySelector(".modal");
-    var inner = document.getElementsByClassName("inner");
-    var card = document.getElementsByClassName("card");
-    var column = document.getElementsByClassName("column");
-    var remove = document.getElementsByClassName("close");
     var confirmation = document.querySelector(".confirmation");
-    var cancel = document.querySelector(".calcel");
-    var add = document.querySelector(".add-task-btn");
     var addition = document.querySelector(".addition");
 
     //Todo el contenido en un array para que sea dinámico y poner añadir/borrar
@@ -91,15 +85,13 @@ $(document).ready(function(){
             }
         }
     //Borramos la columna auxiliar
-        $('.column:first').remove();
+        $('.column:first').hide();
     });
 
     //Borrar la tarjeta en la x
-    for (var i=0; i < remove.length; i++) {
-        remove[i].onclick = function() {
-            modal.classList.toggle("show-modal");
-        };
-    }
+    $('div.inner').on('click', 'div.close', function() {
+        modal.classList.toggle("show-modal");
+    });
 
     function popup() {
         modal.classList.toggle("show-modal");
@@ -124,65 +116,90 @@ $(document).ready(function(){
     //Para clicar fuera del pop up
     window.addEventListener("click", windowOnClick);
 
+    var current_task;
     //Abrir el popup desde cualquier x
     $('div.inner').on('click', 'button.close', function() {
+        current_task = $(this).parent();
         confirmation.classList.toggle("show-confirmation");
-    });/*
-    for (var i=0; i < remove.length; i++) {
-        remove[i].onclick = function() {
-            confirmation.classList.toggle("show-confirmation");
-        };
-    }*/
-/*
+    });
+
     //Cerrar el popup en cancel
-    for (var i=0; i < cancel.length; i++) {
-        cancel[i].onclick = function() {
-            confirmation.classList.toggle("show-confirmation");
-        };
-    }
+    $('div.confirmation').on('click', '#cancel', function() {
+        confirmation.classList.toggle("show-confirmation");
+    });
 
-    function remove(elem){
+    //En confirm cerramos el popup y borramos la tarjeta
+    $('div.confirmation').on('click', '#confirm', function() {
+        //cards.splice(cards.indexOf($(current_task).find('.card-text')), 1);
+        cards[0][1].splice(1, 1);
+        alert(cards);
+        confirmation.classList.toggle("show-confirmation");
+    });
 
-    }
+    //Re-render page (?)
+    $(function(){
+        $('.column:first').show();
+        $('.column:not(:first)').remove();
+        var it = 1;
+        for(i=0; i<cards.length; i++){
+            //Creamos una columna
+            $('.column:first').clone(true, true).appendTo('.row');
+            //Le ponemos el título correspondiente
+            document.getElementsByClassName("card-title")[i+1].innerHTML = cards[i][0];
+            for(j=0; j<cards[i][1].length; j++){
+                //La primera ya existe, por lo que no la duplicamos
+                if(j!=0){
+                    $('.inner:last').clone(true, true).appendTo('.card:last');
+                }
+                //Añadimos el resto del contenido
+                document.getElementsByClassName("card-image")[it].src=cards[i][1][j][1];
+                document.getElementsByClassName("card-text")[it].innerHTML = cards[i][1][j][2];
+                document.getElementsByClassName("card-date")[it].innerHTML = cards[i][1][j][3];
+                it++;
+            }
+        }
+    //Borramos la columna auxiliar
+        $('.column:first').hide();
+    });
 
-    document.getElementById('confirm').addEventListener('click', remove());
-*/
     //Abrir el popup de adición desde cualquier add task
     $('button.btn').on('click', function() {
         addition.classList.toggle("show-addition");
     });
-/*
-    //Cerrar el popup en cancel
-    for (var i=0; i < cancel.length; i++) {
-        cancel[i].onclick = function() {
-            addition.classList.toggle("show-addition");
-        };
-    }
 
-*/
+    //Cerrar el popup en cancel
+    $('div.addition').on('click', '#cancel', function() {
+        addition.classList.toggle("show-addition");
+    });
+
+    //En confirm cerramos el popup y borramos la tarjeta
+    $('div.addition').on('click', '#confirm', function() {
+        addition.classList.toggle("show-addition");
+    });
+
     //Botón like con contador
     $(".like_button").on("click", function() {
-    var $like_count = $(this).parent().find('.like_count');
-    $count.html($like_count.html() + 1);
+        var $like_count = $(this).parent().find('.like_count');
+        $count.html($like_count.html() + 1);
     });
 
 
     //Botón kebab menu
     function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+        document.getElementById("myDropdown").classList.toggle("show");
     }
 
     // Close the dropdown if the user clicks outside of it
     window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
         }
-        }
-    }
     }
 });
