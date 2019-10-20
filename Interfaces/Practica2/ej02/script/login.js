d10b69b7bad592ef2a29a948c6909826ad85e4d1
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-10-20 02:45:26 +0200

    Icon test

--- Interfaces/Practica2/ej02/script/login.js
+++ Interfaces/Practica2/ej02/script/login.js
@@ -3,3 +3,25 @@ $(document).ready(function(){
         alert('   Too bad \n ¯\\_(•_•)_/¯');
     })
 });
+
+function show() {
+    var p = document.getElementById('pwd');
+    p.setAttribute('type', 'text');
+}
+
+function hide() {
+    var p = document.getElementById('pwd');
+    p.setAttribute('type', 'password');
+}
+
+var pwShown = 0;
+
+document.getElementById("eye").addEventListener("click", function () {
+    if (pwShown == 0) {
+        pwShown = 1;
+        show();
+    } else {
+        pwShown = 0;
+        hide();
+    }
+}, false);
