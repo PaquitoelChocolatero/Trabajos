0e9ef99b03f0ad5028fee9abc83be569aae86876
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2019-10-20 21:27:29 +0200

    SignIn/SignUp

--- Interfaces/Practica2/ej02/script/login.js
+++ Interfaces/Practica2/ej02/script/login.js
@@ -1,27 +1,48 @@
 $(document).ready(function(){
+    
     $('#forgotten').on('click', function(){
         alert('   Too bad \n ¯\\_(•_•)_/¯');
     })
+
+    $('div.options').on('click', '.SI', function(){
+        $('div.Sign-Up').remove();
+        $('div.Sign-In').remove();
+        $('.central').append(`
+        <div class='Sign-In'>
+            <form id='info'>
+                <input id="user" type="text" placeholder="Email" required>
+                <br />
+                <br />
+                <input id="pass" type="password" maxlength='8' pattern='[A-Za-z0-9]+' placeholder="Password" required>
+                <button class='go'>Sign In</button>
+                <a id='forgotten'>Forgot your password?</a>
+            </form>
+        </div>        
+        `);
+    })
+
+    $('div.options').on('click', '.SU', function(){
+        $('div.Sign-In').remove();
+        $('div.Sign-Up').remove();
+        $('.central').append(`
+        <div class='Sign-Up'>
+            <form id='info'>
+                <input id="user" type="text" placeholder="Email" required>
+                <br />
+                <br />
+                <input id="pass" type="password" maxlength='8' pattern='[A-Za-z0-9]+' placeholder="Password" required>
+                <br />
+                <br />
+                <div class='name'>
+                    <input type="text" placeholder="Name" required>
+                    <input type="text" placeholder="Last name" required>
+                </div>
+                <br />
+                <br />
+                <button class='go'>Sign Up</button>
+            </form>
+        </div>        
+        `);
+    })
+
 });
-
-function show() {
-    var p = document.getElementById('pwd');
-    p.setAttribute('type', 'text');
-}
-
-function hide() {
-    var p = document.getElementById('pwd');
-    p.setAttribute('type', 'password');
-}
-
-var pwShown = 0;
-
-document.getElementById("eye").addEventListener("click", function () {
-    if (pwShown == 0) {
-        pwShown = 1;
-        show();
-    } else {
-        pwShown = 0;
-        hide();
-    }
-}, false);
