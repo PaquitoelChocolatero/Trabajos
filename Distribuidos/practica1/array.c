83f31f33daf89fa6ba725dd84bba63bd5435b9ad
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2020-02-24 12:00:48 +0100

    Init (?)

--- Distribuidos/practica1/array.c
+++ Distribuidos/practica1/array.c
@@ -2,6 +2,7 @@
 
 //Lista enlazada de vectores
 typedef struct node {
+    char *name;
     int *vector;
     struct node *next;
 } nodeList;
@@ -10,23 +11,34 @@ typedef struct node {
 nodeList *linkedList;
 
 //Función que añade nodos a la lista enlazada
-void add (nodeList *node, int *vector)
+void add (nodeList *node)
 {
     if(linkedList->vector == NULL)  //Añadimos siguiente nodo, desplazamos cabeza y ponemos valor
     {
         linkedList->next=node;
         linkedList=node;
-        linkedList->vector=vector;
+        strcpy(linkedList->name, node->name);
+        linkedList->vector=node->vector;
     }
     else  //Si es el primer nodo sólo añadimos el vector
     {
-        linkedList->vector=vector;
+        linkedList->vector=node->vector;
     }
 }
 
 int init (char *nombre, int N)
 {
-    return 0;
+    while(linkedList->next != NULL)
+    {
+        if(strcmp(linkedList->name, nombre) == 1) return 0;
+        linkedList=linkedList->next;
+    }
+    nodeList *node;
+    strcpy(node->name, nombre);
+    int vector[N];
+    node->vector=vector;
+    if(!add(&node)) return -1;
+    return 1;
 }
 
 int set (char *nombre, int i, int valor)
