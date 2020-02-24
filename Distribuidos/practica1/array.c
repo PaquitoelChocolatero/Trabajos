ec002dbbbd7d5e84f3c4d37c82780b9d2c77498f
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2020-02-24 10:12:58 +0100

    Cabeceras

---
+++ Distribuidos/practica1/array.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+int init (char *nombre, int N)
+{
+    int *nombre
+    return 0;
+}
+
+int set (char *nombre, int i, int valor)
+{
+    return 0;
+}
+
+int get (char *nombre, int i, int *valor)
+{
+    return 0;
+}
+
+int destroy (char *nombre)
+{
+    return 0;
+}
---
+++ Distribuidos/practica1/array.h
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+
+int init (char *nombre, int N);
+int set (char *nombre, int i, int valor);
+int init (char *nombre, int i, int *valor);
+int destroy (char *nombre);
