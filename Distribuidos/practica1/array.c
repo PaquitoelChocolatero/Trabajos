e802841c4b5939008ce0c59be1ac30c2746ace8c
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2020-02-26 12:48:36 +0100

    Array.c done

--- Distribuidos/practica1/array.c
+++ Distribuidos/practica1/array.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 //Lista enlazada de vectores
 typedef struct node {
@@ -9,15 +11,16 @@ typedef struct node {
 
 //Inicializamos la lista enlazada
 nodeList *linkedList;
+nodeList *auxlinkedList;
 
 //Función que añade nodos a la lista enlazada
 void add (nodeList *node)
 {
-    if(linkedList->vector == NULL)  //Añadimos siguiente nodo, desplazamos cabeza y ponemos valor
+    if(linkedList->vector != NULL)  //Añadimos siguiente nodo, desplazamos cabeza y ponemos valor
     {
         linkedList->next=node;
         linkedList=node;
-        strcpy(linkedList->name, node->name);
+        linkedList->name = node->name;
         linkedList->vector=node->vector;
     }
     else  //Si es el primer nodo sólo añadimos el vector
@@ -30,11 +33,11 @@ int init (char *nombre, int N)
 {
     while(linkedList->next != NULL)
     {
-        if(strcmp(linkedList->name, nombre) == 1) return 0;
+        if(strcmp(linkedList->name, nombre) == 0) return 0;
         linkedList=linkedList->next;
     }
     nodeList *node;
-    strcpy(node->name, nombre);
+    node->name = nombre;
     int vector[N];
     node->vector=vector;
     if(!add(&node)) return -1;
@@ -43,15 +46,61 @@ int init (char *nombre, int N)
 
 int set (char *nombre, int i, int valor)
 {
-    return 0;
+    while(linkedList->next != NULL)
+    {
+        if(strcmp(linkedList->name, nombre) == 0)
+        {
+            for(int j=0; j<sizeof(linkedList->vector)/sizeof(char); j++)
+            {
+                if(j==i) 
+                {
+                    linkedList->vector[j]=valor;
+                    return 0;
+                }
+            }
+        }
+    }
+    return -1;
 }
 
 int get (char *nombre, int i, int *valor)
 {
-    return 0;
+    while(linkedList->next != NULL)
+    {
+        if(strcmp(linkedList->name, nombre) == 0)
+        {
+            for(int j=0; j<sizeof(linkedList->vector)/sizeof(char); j++)
+            {
+                if(j==i) 
+                {
+                    valor = linkedList->vector[j];
+                    return 0;
+                   
+                }
+
+            }
+        }
+    }
+    return -1;
 }
 
 int destroy (char *nombre)
 {
-    return 0;
+    while(linkedList->next != NULL)
+    {
+        if(strcmp(linkedList->name, nombre) == 0)
+        {
+            struct node *aux = linkedList->next;
+            free(linkedList->vector);
+            free(linkedList);
+            
+            nodeList *linkedList = auxlinkedList;
+            while(linkedList->next != NULL)
+            {
+                linkedList->next = aux;
+                return 0;
+            }
+        }
+    }
+    return -1;
 }
