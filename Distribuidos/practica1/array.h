0632297e5b51898ca41b853c070970b2428cc637
danidef99 <dani.defrutosdiez@gmail.com>
2020-02-26 17:26:09 +0100

    inicio de servidor.c

--- Distribuidos/practica1/array.h
+++ Distribuidos/practica1/array.h
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-
 int init (char *nombre, int N);
 int set (char *nombre, int i, int valor);
 int get (char *nombre, int i, int *valor);
--- Distribuidos/practica1/servidor.c
+++ Distribuidos/practica1/servidor.c
@@ -1,1 +1,22 @@
 #include "array.h"
+#include <mqueue.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_MESSAGES 10
+
+int main(){
+
+    mqd_t serverQueue;
+    struct mq_attr atr;
+    atr.mq_maxmsg=MAX_MESSAGES;
+    atr.mq_msgsize=sizeof(int);
+    if((serverQueue=mq_open("/serverQueue.txt", O_CREAT|O_WRONLY, 0777, &atr))==-1){
+        printf("ERROR MQ_OPEN");
+        exit(-1);
+    }
+
+
+    mq_close(serverQueue);
+    mq_unlink("/serverQueue.txt");
+}
