57850d1d97a1c765bbc1d3fc0bb211b56190242a
danidef99 <dani.defrutosdiez@gmail.com>
2020-03-21 17:25:46 +0100

    pruebitas

--- Distribuidos/practica1/cliente.c
+++ Distribuidos/practica1/cliente.c
@@ -7,20 +7,41 @@ int main(void)
 
     init("vector2", 20);
 
+    init("vector1", 10);
+
+    init("vector2", 20);
+
+    init("vector1", 15);
+
+    init("vector2", 25);
+
     set("vector1", 0, 44);
 
-    sleep(2);
-
     set("vector1", 12, 30);
+
+    set("vector2", 5, 22);
+
+    set("vector2", 24, 50);
+
+    set("vector3", 0, 47);
 
     int valor;
     get("vector1", 0, &valor);
 
-    init("vector1", 20);
+    get("vector1", 54, &valor);
+
+    get("vector3", 54, &valor);
+
+    int valor2;
+    get("vector2", 0, &valor2);
+
+    get("vector2", 54, &valor2);
+
+    get("vector3", 1, &valor2);
 
     destroy("vector1");
 
-    destroy("vector");
+    destroy("vector3");
 
     destroy("vector2");
 
