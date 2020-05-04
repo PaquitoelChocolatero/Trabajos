31a7f4b941d5fdec116b43fbccb48d4a6a6e1b17
PaquitoElChocolatero <alexdelacruzalv@gmail.com>
2020-05-04 22:13:12 +0200

    List content remote user not connected

--- Distribuidos/practica2/operations.c
+++ Distribuidos/practica2/operations.c
@@ -643,48 +643,33 @@ int list_content(char *user, char *sourceUser, char *** list)
             //Si el usuario existe
             if(exists == 1){
                 exists = 0;
-        
-                //Comprobar si el usuario está conectado
-                strcpy(concat_sql_op, "SELECT * FROM USERS WHERE user='");
+
+                //Listar archivos del usuario
+                strcpy(concat_sql_op, "SELECT * FROM FILES WHERE user='");
                 strcat(concat_sql_op, user);
                 strcat(concat_sql_op, "';");
-                active_rc = sqlite3_exec(active_db, concat_sql_op, elementExists, 0, &err);
+                active_rc = sqlite3_prepare_v2(active_db, concat_sql_op, -1, &res, 0);
                 checkError();
-        
-                //Si el usuario está conectado
-                if(exists == 1){
-                    exists = 0;
 
-                    //Listar archivos del usuario
-                    strcpy(concat_sql_op, "SELECT * FROM FILES WHERE user='");
-                    strcat(concat_sql_op, user);
-                    strcat(concat_sql_op, "';");
-                    active_rc = sqlite3_prepare_v2(active_db, concat_sql_op, -1, &res, 0);
-                    checkError();
+                active_rc = sqlite3_step(res);
+
+                int i = 0;
+                while(active_rc == SQLITE_ROW) {
+                    //Añadimos espacio en el array para la nueva posición
+                    if(results==1){
+                        results = 2;
+                        *list = (char **) malloc(results * sizeof(char *));
+                    }else{
+                        results+=2;
+                        *list = (char **) realloc((*list), results * sizeof(*list));
+                    }
+                    (*list)[i] = malloc(256);
+                    (*list)[i+1] = malloc(256);
+                    sprintf((*list)[i], "%s", sqlite3_column_text(res, 1));
+                    sprintf((*list)[i+1], "%s", sqlite3_column_text(res, 2));
 
                     active_rc = sqlite3_step(res);
-
-                    int i = 0;
-                    while(active_rc == SQLITE_ROW) {
-                        //Añadimos espacio en el array para la nueva posición
-                        if(results==1){
-                            results = 2;
-                            *list = (char **) malloc(results * sizeof(char *));
-                        }else{
-                            results+=2;
-                            *list = (char **) realloc((*list), results * sizeof(*list));
-                        }
-                        (*list)[i] = malloc(256);
-                        (*list)[i+1] = malloc(256);
-                        sprintf((*list)[i], "%s", sqlite3_column_text(res, 1));
-                        sprintf((*list)[i+1], "%s", sqlite3_column_text(res, 2));
-
-                        active_rc = sqlite3_step(res);
-                        i+=2;
-                    }
-                }else{
-                    printf("S> LIST_CONTENT FAIL, REMOTE USER NOT CONNECTED\n");
-                    return -4;
+                    i+=2;
                 }
             }else{
                 printf("S> LIST_CONTENT FAIL, REMOTE USER DOES NOT EXIST\n");
