b36b130d6d87b8a44fb074a30ff0b783610988ed
juanbanpar <100383373@alumnos.uc3m.es>
2020-10-17 12:33:10 +0200

    Plantilla cargada y esbozo de login hecho

---
+++ TIW/Chiquify/src/g16/servlets/LoginServlet.java
@@ -0,0 +1,78 @@
+package g16.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#getServletConfig()
+	 */
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		
+		Usuario _usuario = new Usuario();
+        _usuario.setUsername(request.getParameter("username"));
+        _usuario.setPassword(request.getParameter("password"));
+        
+        HttpSession session = request.getSession(true);
+        session.setAttribute("user", _usuario);
+
+        RequestDispatcher reqDis =
+            request.getRequestDispatcher("index.html");
+        reqDis.forward(request, response);
+	}
+
+}
---
+++ TIW/Chiquify/src/g16/servlets/Usuario.java
@@ -0,0 +1,68 @@
+package g16.servlets;
+
+public class Usuario {
+	String name;
+	String lastName1;
+	String lastName2;
+	String city;
+    String username;
+    String email;
+    String password;
+
+    public void setName(String value) {
+        name = value;
+    }
+    
+    public void setLastName1(String value) {
+        lastName1 = value;
+    }
+    
+    public void setLastName2(String value) {
+        lastName2 = value;
+    }
+    
+    public void setCity(String value) {
+        city = value;
+    }
+    
+    public void setUsername(String value) {
+        username = value;
+    }
+
+    public void setEmail(String value) {
+        email = value;
+    }
+    
+    public void setPassword(String value) {
+        password = value;
+    }
+
+	public String getName() {
+		return name;
+	}
+
+	public String getLastName1() {
+		return lastName1;
+	}
+
+	public String getLastName2() {
+		return lastName2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+}
