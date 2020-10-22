01a0e49871ba5a04921478a0d4f5f54de1f977e2
juanbanpar <100383373@alumnos.uc3m.es>
2020-10-22 18:56:14 +0200

    Registration Servlet

---
+++ TIW/Chiquify/src/g16/servlets/RegistrationServlet.java
@@ -0,0 +1,83 @@
+package g16.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class RegistrationServlet
+ */
+@WebServlet("/RegistrationServlet")
+public class RegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegistrationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#getServletConfig()
+	 */
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		
+		Usuario _usuario = new Usuario();
+		_usuario.setName(request.getParameter("name"));
+		_usuario.setLastName1(request.getParameter("lastname1"));
+		_usuario.setLastName2(request.getParameter("lastname2"));
+		_usuario.setCity(request.getParameter("city"));
+        _usuario.setUsername(request.getParameter("username"));
+		_usuario.setEmail(request.getParameter("email"));
+        _usuario.setPassword(request.getParameter("password"));
+        
+        HttpSession session = request.getSession(true);
+        session.setAttribute("user", _usuario);
+
+        RequestDispatcher reqDis =
+            request.getRequestDispatcher("index.html");
+        reqDis.forward(request, response);
+	}
+
+}
